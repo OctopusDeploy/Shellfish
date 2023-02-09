e6baa2278c57acc69b639a05fbed591b54342139
Matt Richardson <matt.richardson@octopus.com>
2023-02-09 16:58:07 +1100

    Auto generate teamcity config [wip]

--- build/Build.cs
+++ build/Build.cs
@@ -2,10 +2,13 @@
 
 using System;
 using Nuke.Common;
+using Nuke.Common.CI.TeamCity;
+using Nuke.Common.CI.TeamCity.Configuration;
 using Nuke.Common.Execution;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.Octopus;
 using Nuke.Common.Tools.OctoVersion;
 using Nuke.Common.Utilities.Collections;
 using Serilog;
@@ -14,6 +17,24 @@ using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
 [CheckBuildProjectConfigurations]
 [UnsetVisualStudioEnvironmentVariables]
+[TeamCity(
+    Description = "",
+    AutoGenerate = true,
+    ExcludedTargets = new []
+    {
+        nameof(Clean),
+        nameof(Restore),
+        nameof(CopyToLocalPackages),
+        nameof(Publish),
+        nameof(Pack),
+        nameof(Compile),
+        nameof(Test),
+    },
+    NightlyBuildAlways = true,
+    NightlyTriggeredTargets = new [] { nameof(ChainNightlyBuild)},
+    VcsTriggeredTargets = new [] { nameof(ChainBuildAndTestAndPublish)}
+    
+)]
 class Build : NukeBuild
 {
     const string CiBranchNameEnvVariable = "OCTOVERSION_CurrentBranch";
@@ -31,7 +52,13 @@ class Build : NukeBuild
 
     [Parameter("Branch name for OctoVersion to use to calculate the version number. Can be set via the environment variable " + CiBranchNameEnvVariable + ".", Name = CiBranchNameEnvVariable)]
     string BranchName { get; set; }
-
+    
+    [Parameter("The Octopus Server to publish to")]
+    public string OctopusServerUrl { get; set; }
+    [Parameter("The ApiKey to use to authenticate to Octopus Server")]
+    [Secret]
+    public string OctopusServerApiKey { get; set; }
+    
     AbsolutePath SourceDirectory => RootDirectory / "source";
     AbsolutePath ArtifactsDirectory => RootDirectory / "artifacts";
     AbsolutePath LocalPackagesDirectory => RootDirectory / ".." / "LocalPackages";
@@ -59,6 +86,7 @@ class Build : NukeBuild
         });
 
     Target Compile => _ => _
+        .DependsOn(CalculateVersion)
         .DependsOn(Clean)
         .DependsOn(Restore)
         .Executes(() =>
@@ -84,8 +112,9 @@ class Build : NukeBuild
         });
 
     Target Pack => _ => _
-        .DependsOn(Compile)
-        .DependsOn(Test)
+        .DependsOn(BuildAndTestWindows)
+        .Consumes(BuildAndTestWindows, "*") //this can go :boom: if the target is not specified by `.DependsOn()`
+        .Produces("artifacts/*") //this feels like it should be on Pack, not here
         .Executes(() =>
         {
             DotNetPack(_ => _
@@ -106,6 +135,59 @@ class Build : NukeBuild
             CopyFileToDirectory(ArtifactsDirectory / $"Octopus.Shellfish.{OctoVersionInfo.FullSemVer}.nupkg", LocalPackagesDirectory, FileExistsPolicy.Overwrite);
         });
 
+    /************************/
+    /** Testing nuke generation **/
+    /************************/
+    
+
+    Target TestLinux => _ => _
+        .DependsOn(Compile)
+        .DependsOn(Test);
+
+    Target BuildAndTestWindows => _ => _
+        .DependsOn(Compile)
+        .DependsOn(Test)
+        ;
+
+    Target Publish => _ => _
+        .OnlyWhenStatic(() => !IsLocalBuild)
+        .DependsOn(Pack)
+        .Consumes(Pack, "*")
+        .Executes(() =>
+        {
+            OctopusTasks.OctopusBuildInformation(s => s
+                .SetServer(OctopusServerUrl)
+                .SetApiKey(OctopusServerApiKey)
+                .SetSpace("Core Platform")
+                .SetPackageId("Octopus.Shellfish")
+                .SetVersion(OctoVersionInfo.FullSemVer)
+            );
+            OctopusTasks.OctopusPush(s => s
+                .SetServer(OctopusServerUrl)
+                .SetApiKey(OctopusServerApiKey)
+                .SetSpace("Core Platform")
+                .SetPackage($"**/Octopus.Shellfish.{OctoVersionInfo.FullSemVer}.nupkg")
+            );
+            OctopusTasks.OctopusCreateRelease(s => s
+                .SetServer(OctopusServerUrl)
+                .SetApiKey(OctopusServerApiKey)
+                .SetSpace("Core Platform")
+                .SetProject("Octopus.Shellfish")
+                .SetVersion(OctoVersionInfo.FullSemVer)
+            );
+        });
+
+    Target ChainBuildAndTestAndPublish => _ => _
+        .DependsOn(TestLinux)
+        .DependsOn(BuildAndTestWindows)
+        .DependsOn(Publish);
+    
+    Target ChainNightlyBuild => _ => _
+        .DependsOn(CalculateVersion) //we need to generate a "always re-run" here
+        .DependsOn(TestLinux)
+        .DependsOn(BuildAndTestWindows)
+        .DependsOn(Publish);
+    
     /// Support plugins are available for:
     /// - JetBrains ReSharper        https://nuke.build/resharper
     /// - JetBrains Rider            https://nuke.build/rider
