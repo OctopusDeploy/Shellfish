09aeb8def67c712d1a35878f6e6c3d203c7198ed
borland <orion.edwards@gmail.com>
2024-11-21 09:30:47 +1300

    code cleanup, InputQueue knows when it's shut down

--- source/Shellfish/InputQueue.cs
+++ source/Shellfish/InputQueue.cs
@@ -11,26 +11,29 @@ namespace Octopus.Shellfish;
 // it asynchronously in the background
 public class InputQueue : IInputSourceObserver
 {
+    readonly Queue<Notification> queue = new(); // serves as the lock-object for this instance
     readonly StreamWriter processStdInput;
-    readonly Queue<Notification> queue = new();
     TaskCompletionSource<bool> wakeupSignal = new();
+    Task? messagePumpTask; // set to null when the message pump shuts down, so we can avoid enqueuing more messages
 
     public InputQueue(StreamWriter processStdInput)
     {
         this.processStdInput = processStdInput;
-        _ = BeginMessagePump();
+        messagePumpTask = BeginMessagePump();
     }
-    
+
     public void OnNext(string line)
     {
         TaskCompletionSource<bool> sig;
-        lock(queue)
+        lock (queue)
         {
+            if (messagePumpTask == null) return;
+
             queue.Enqueue(new(NotificationType.Next, line));
-            
+
             // it is very wasteful to allocate a new TaskCompletionSource for every line, but it's simple.
             // Shellfish only deals with a few lines of input so we can accept the compromise.
-            sig = wakeupSignal; 
+            sig = wakeupSignal;
             wakeupSignal = new();
         }
 
@@ -40,8 +43,10 @@ public class InputQueue : IInputSourceObserver
     public void OnCompleted()
     {
         TaskCompletionSource<bool> sig;
-        lock(queue)
+        lock (queue)
         {
+            if (messagePumpTask == null) return;
+
             queue.Enqueue(new(NotificationType.Completed));
             sig = wakeupSignal;
             wakeupSignal = new();
@@ -49,47 +54,64 @@ public class InputQueue : IInputSourceObserver
 
         sig.TrySetResult(true);
     }
-    
+
     async Task BeginMessagePump()
     {
-        while (true)
+        try
         {
-            Notification notification;
+            while (true)
+            {
+                Notification notification;
+                lock (queue)
+                {
+                    notification = queue.Count > 0 ? queue.Dequeue() : default;
+                }
+
+                switch (notification.Type)
+                {
+                    case NotificationType.Empty: //  wait for the next wakeup signal
+                    {
+                        TaskCompletionSource<bool> sig;
+                        lock (queue)
+                        {
+                            sig = wakeupSignal;
+                        }
+
+                        await sig.Task;
+                        continue; // go round again
+                    }
+
+                    case NotificationType.Next when notification.Line is not null: // onNext
+                        await processStdInput.WriteLineAsync(notification.Line);
+                        await processStdInput.FlushAsync();
+                        break;
+
+                    case NotificationType.Completed:
+                        processStdInput.Close();
+                        return; // exit the entire message pump
+
+                    // Normally we would have a default: case which logged or threw an "Unhandled case" exception,
+                    // but we are a background task, there's nobody to observe such a thing.
+                }
+            }
+        }
+        finally
+        {
             lock (queue)
             {
-                notification = queue.Count > 0 ? queue.Dequeue() : default;
-            }
-
-            switch (notification.Type)
-            {
-                case NotificationType.Empty: //  wait for the next wakeup signal
-                {
-                    TaskCompletionSource<bool> sig;
-                    lock (queue)
-                    {
-                        sig = wakeupSignal;
-                    }
-                    await sig.Task;
-                    continue; // go round again
-                }
-                
-                case NotificationType.Next when notification.Line is not null: // onNext
-                    await processStdInput.WriteLineAsync(notification.Line);
-                    await processStdInput.FlushAsync();
-                    break;
-                
-                case NotificationType.Completed:
-                    processStdInput.Close();
-                    return; // exit the entire message pump
-
-                // Normally we would have a default: case which logged or threw an "Unhandled case" exception,
-                // but we are a background task, there's nobody to observe such a thing.
+                messagePumpTask = null;
             }
         }
     }
 
     // the correct way to model this is with a Notification base-class and inherited Next/Completed classes,
     // but we only have two cases so we can use a struct to avoid allocations
-    enum NotificationType { Empty, Next, Completed }
+    enum NotificationType
+    {
+        Empty,
+        Next,
+        Completed
+    }
+
     record struct Notification(NotificationType Type, string? Line = null);
 }
