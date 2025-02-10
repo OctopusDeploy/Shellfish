b642eec103775ae084b7ba02505482847582b92d
Adam McCoy <adam.mccoy@octopus.com>
2025-02-10 17:53:57 +1100

    Refactor ShellCommand (#137)
    * Split process config and execution
    
    * Fix comment

---
+++ source/Shellfish/ProcessExtensions.cs
@@ -0,0 +1,20 @@
+﻿#if !NET5_0_OR_GREATER 
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Octopus.Shellfish;
+
+static class ProcessExtensions
+{
+    // WaitForExitAsync was added in net5. It handles the buffer flushing scenario so we can simply call it.
+    public static async Task WaitForExitAsync(this Process process, CancellationToken cancellationToken)
+    {
+        // Compatibility shim for netstandard2.0
+        // Similarly to the sync version, We want to wait for the StdErr and StdOut streams to flush but cannot easily
+        // do this ourselves. https://github.com/dotnet/runtime/blob/e03b9a4692a15eb3ffbb637439241e8f8e5ca95f/src/libraries/System.Diagnostics.Process/src/System/Diagnostics/Process.cs#L1565
+        await Task.CompletedTask;
+        process.WaitForExit(); // note we CANNOT pass a timeout here as otherwise the WaitForExit implementation will not wait for the streams to flush
+    }
+}
+#endif
--- source/Shellfish/ShellCommand.cs
+++ source/Shellfish/ShellCommand.cs
@@ -9,8 +9,6 @@ using System.Threading.Tasks;
 
 namespace Octopus.Shellfish;
 
-using static ShellCommandExecutorHelpers;
-
 /// <summary>
 /// A fluent-builder style utility designed to easily let you execute shell commands.
 /// </summary>
@@ -166,51 +164,24 @@ public class ShellCommand
     /// </summary>
     public ShellCommandResult Execute(CancellationToken cancellationToken = default)
     {
-        using var process = new Process();
-        ConfigureProcess(process, out var shouldBeginOutputRead, out var shouldBeginErrorRead, out var shouldBeginInput);
+        using var process = new ShellfishProcess(
+            executable,
+            argumentString,
+            argumentList,
+            workingDirectory,
+            environmentVariables,
+            windowsCredential,
+            commandOptions,
+            onCaptureProcess,
+            outputEncoding,
+            stdOutTargets,
+            stdErrTargets,
+            stdInSource);
 
-        var exitedEvent = AttachProcessExitedManualResetEvent(process, cancellationToken);
-        process.Start();
+        process.Start(cancellationToken);
+        process.WaitForExit(cancellationToken);
 
-        onCaptureProcess?.Invoke(process);
-
-        IDisposable? closeStdInDisposable = BeginIoStreams(process, shouldBeginOutputRead, shouldBeginErrorRead, shouldBeginInput);
-
-        try
-        {
-            exitedEvent?.Wait(cancellationToken);
-
-            // Either: AttachProcessExitedManualResetEvent determined there was no cancellation to consider and exitedEvent is null.
-            // We can offload all the work to process.WaitForExit();
-            //
-            // Or: ExitEvent was signalled, but we still want to call process.WaitForExit; it waits for the StdErr and StdOut streams to flush.
-            // in a way that can we cannot easily do ourselves.  https://github.com/microsoft/referencesource/blob/51cf7850defa8a17d815b4700b67116e3fa283c2/System/services/monitoring/system/diagnosticts/Process.cs#L2453
-            // It should return very quickly.
-            if (!cancellationToken.IsCancellationRequested) process.WaitForExit(); // note we CANNOT pass a timeout here as otherwise the WaitForExit implementation will not wait for the streams to flush
-        }
-        catch (OperationCanceledException) when (cancellationToken.IsCancellationRequested)
-        {
-            // Cancellation is not necessarily an error, and some processes will gracefully exit themselves when stdin is closed.
-            // We can observe a valid 0 exit code if that happens.
-            // If the process does not close itself, we'll proceed to killing it.
-            closeStdInDisposable?.Dispose();
-            closeStdInDisposable = null;
-
-            if (shouldBeginOutputRead) process.CancelOutputRead();
-            if (shouldBeginErrorRead) process.CancelErrorRead();
-
-            // The legacy ShellExecutor would unconditionally kill the process upon cancellation.
-            // We keep that default as it is the safest option for compatibility
-            TryKillProcessAndChildrenRecursively(process);
-
-            if (commandOptions != ShellCommandOptions.DoNotThrowOnCancellation) throw;
-        }
-        finally
-        {
-            closeStdInDisposable?.Dispose();
-        }
-
-        return new ShellCommandResult(SafelyGetExitCode(process));
+        return new ShellCommandResult(process.SafelyGetExitCode());
     }
 
     /// <summary>
@@ -218,174 +189,23 @@ public class ShellCommand
     /// </summary>
     public async Task<ShellCommandResult> ExecuteAsync(CancellationToken cancellationToken = default)
     {
-        using var process = new Process();
-        ConfigureProcess(process, out var shouldBeginOutputRead, out var shouldBeginErrorRead, out var shouldBeginInput);
+        using var process = new ShellfishProcess(
+            executable,
+            argumentString,
+            argumentList,
+            workingDirectory,
+            environmentVariables,
+            windowsCredential,
+            commandOptions,
+            onCaptureProcess,
+            outputEncoding,
+            stdOutTargets,
+            stdErrTargets,
+            stdInSource);
 
-        var exitedTask = AttachProcessExitedTask(process, cancellationToken);
-        process.Start();
-        
-        onCaptureProcess?.Invoke(process);
+        process.Start(cancellationToken);
+        await process.WaitForExitAsync(cancellationToken);
 
-        IDisposable? closeStdInDisposable = BeginIoStreams(process, shouldBeginOutputRead, shouldBeginErrorRead, shouldBeginInput);
-
-        try
-        {
-            // tests deadlock on linux if we ConfigureAwait(false) here
-            await exitedTask.ConfigureAwait(true);
-
-            if (!cancellationToken.IsCancellationRequested) await FinalWaitForExitAsync(process, cancellationToken).ConfigureAwait(false);
-        }
-        catch (OperationCanceledException) when (cancellationToken.IsCancellationRequested)
-        {
-            // Cancellation is not necessarily an error, and some processes will gracefully exit themselves when stdin is closed.
-            // We can observe a valid 0 exit code if that happens.
-            // If the process does not close itself, we'll proceed to killing it.
-            closeStdInDisposable?.Dispose();
-            closeStdInDisposable = null;
-
-            if (shouldBeginOutputRead) process.CancelOutputRead();
-            if (shouldBeginErrorRead) process.CancelErrorRead();
-
-            // The legacy ShellExecutor would unconditionally kill the process upon cancellation.
-            // We keep that default as it is the safest option for compatibility
-            TryKillProcessAndChildrenRecursively(process);
-
-            if (commandOptions != ShellCommandOptions.DoNotThrowOnCancellation) throw;
-        }
-        finally
-        {
-            closeStdInDisposable?.Dispose();
-        }
-
-        return new ShellCommandResult(SafelyGetExitCode(process));
-    }
-
-    // sets standard flags on the Process that apply for both Execute and ExecuteAsync
-    void ConfigureProcess(Process process, out bool shouldBeginOutputRead, out bool shouldBeginErrorRead, out bool shouldBeginInput)
-    {
-        process.StartInfo.FileName = executable;
-
-        if (argumentString is not null && argumentList is { Count: > 0 }) throw new InvalidOperationException("Cannot specify both raw arguments and arguments");
-
-        if (argumentString is not null)
-        {
-            process.StartInfo.Arguments = argumentString;
-        }
-        else if (argumentList is { Count: > 0 })
-        {
-#if NET5_0_OR_GREATER
-            // Prefer ArgumentList if we're on net5.0 or greater. Our polyfill should have the same behaviour, but
-            // If we stick with the CLR we will pick up optimizations and bugfixes going forward
-            foreach (var arg in argumentList) process.StartInfo.ArgumentList.Add(arg);
-#else
-            process.StartInfo.Arguments = PasteArguments.JoinArguments(argumentList);
-#endif
-        }
-
-        if (workingDirectory is not null) process.StartInfo.WorkingDirectory = workingDirectory;
-        process.StartInfo.UseShellExecute = false;
-        process.StartInfo.CreateNoWindow = true;
-
-        if (outputEncoding is not null)
-        {
-            process.StartInfo.StandardOutputEncoding = outputEncoding;
-            process.StartInfo.StandardErrorEncoding = outputEncoding;
-        }
-
-        if (windowsCredential is not null)
-        {
-            XPlatAdapter.ConfigureStartInfoForUser(process.StartInfo, windowsCredential, environmentVariables);
-        }
-        else // exec as the current user
-        {
-            // Accessing the ProcessStartInfo.EnvironmentVariables dictionary will preload the environment variables for the current process
-            // Then we'll add/overwrite with the customEnvironmentVariables
-            if (environmentVariables is { Count: > 0 })
-            {
-                foreach (var kvp in environmentVariables)
-                {
-                    process.StartInfo.EnvironmentVariables[kvp.Key] = kvp.Value;
-                }
-            }
-        }
-
-        shouldBeginOutputRead = shouldBeginErrorRead = false;
-        if (stdOutTargets is { Count: > 0 })
-        {
-            process.StartInfo.RedirectStandardOutput = true;
-            shouldBeginOutputRead = true;
-
-            var targets = stdOutTargets.ToArray();
-            process.OutputDataReceived += (_, e) =>
-            {
-                if (e.Data is null) return; // don't pass nulls along to the targets, it's an edge case that happens when the process exits
-
-                foreach (var target in targets) target.WriteLine(e.Data);
-            };
-        }
-
-        if (stdErrTargets is { Count: > 0 })
-        {
-            process.StartInfo.RedirectStandardError = true;
-            shouldBeginErrorRead = true;
-
-            var targets = stdErrTargets.ToArray();
-            process.ErrorDataReceived += (_, e) =>
-            {
-                if (e.Data is null) return; // don't pass nulls along to the targets, it's an edge case that happens when the process exits
-
-                foreach (var target in targets) target.WriteLine(e.Data);
-            };
-        }
-
-        shouldBeginInput = false;
-        if (stdInSource is not null)
-        {
-            process.StartInfo.RedirectStandardInput = true;
-            shouldBeginInput = true;
-        }
-    }
-
-    // Common code for Execute and ExecuteAsync to handle stdin and stdout streaming
-    IDisposable? BeginIoStreams(Process process,
-        bool shouldBeginOutputRead,
-        bool shouldBeginErrorRead,
-        bool shouldBeginInput)
-    {
-        if (shouldBeginOutputRead) process.BeginOutputReadLine();
-        if (shouldBeginErrorRead) process.BeginErrorReadLine();
-
-        if (shouldBeginInput && stdInSource != null)
-        {
-            var inputQueue = new InputQueue(process.StandardInput);
-            var unsubscribe = stdInSource.Subscribe(inputQueue);
-            return new StopInputStreamDisposable(inputQueue, unsubscribe);
-        }
-
-        return null;
-    }
-
-    sealed class StopInputStreamDisposable(InputQueue inputQueue, IDisposable additionalDisposable) : IDisposable
-    {
-        public void Dispose()
-        {
-            // tell the input queue to shut itself down
-            inputQueue.OnCompleted();
-            // do whatever other work might be attached
-            additionalDisposable.Dispose();
-        }
-    }
-
-    static async Task FinalWaitForExitAsync(Process process, CancellationToken cancellationToken)
-    {
-#if NET5_0_OR_GREATER // WaitForExitAsync was added in net5. It handles the buffer flushing scenario so we can simply call it.
-        await process.WaitForExitAsync(cancellationToken).ConfigureAwait(false);
-#else
-        // Compatibility shim for netstandard2.0
-        // Similarly to the sync version, We want to wait for the StdErr and StdOut streams to flush but cannot easily
-        // do this ourselves. https://github.com/dotnet/runtime/blob/e03b9a4692a15eb3ffbb637439241e8f8e5ca95f/src/libraries/System.Diagnostics.Process/src/System/Diagnostics/Process.cs#L1565
-        await Task.CompletedTask;
-        process.WaitForExit(); // note we CANNOT pass a timeout here as otherwise the WaitForExit implementation will not wait for the streams to flush
-#endif
+        return new ShellCommandResult(process.SafelyGetExitCode());
     }
 }
